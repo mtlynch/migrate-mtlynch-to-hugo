86eebe79504f0d6e2f9c7094fe78d1972efe9a63
Michael Lynch <git@mtlynch.io>
2019-12-24 23:49:24 -0500

    Adding migration for retrospectives

--- app/projects.py
+++ app/projects.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+import frontmatter
+
 logger = logging.getLogger(__name__)
 
 
@@ -12,25 +14,9 @@ def migrate(old_root, new_root):
     with open(old_projects_path) as old_projects_file:
         old_contents = old_projects_file.read()
         new_contents = old_contents
-        new_contents = _filter_unneeded_frontmatter(new_contents)
+        new_contents = frontmatter.filter_fields(new_contents,
+                                                 field_whitelist=['title'])
 
     new_projects_path = os.path.join(new_root, 'content', 'projects.md')
     with open(new_projects_path, 'w') as new_projects_file:
         new_projects_file.write(new_contents)
-
-
-def _filter_unneeded_frontmatter(contents):
-    triple_underscores = 0
-    lines = []
-    for line in contents.split('\n'):
-        if line.startswith('---'):
-            triple_underscores += 1
-            lines.append(line)
-            continue
-        if triple_underscores == 1:
-            if line.startswith('title:'):
-                pass
-            else:
-                continue
-        lines.append(line)
-    return '\n'.join(lines)
