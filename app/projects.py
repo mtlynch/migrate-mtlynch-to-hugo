924bd84f95d54b618eac2e940a515312268a5d18
Michael Lynch <mtlynch@users.noreply.github.com>
2019-12-25 19:42:41 -0600

    Changing all variable names to 'contents' (#40)

--- app/projects.py
+++ app/projects.py
@@ -12,11 +12,10 @@ def migrate(old_root, new_root):
     old_projects_path = os.path.join(old_root, '_pages', 'projects.md')
 
     with open(old_projects_path) as old_projects_file:
-        old_contents = old_projects_file.read()
-        new_contents = old_contents
-        new_contents = frontmatter.filter_fields(new_contents,
-                                                 field_whitelist=['title'])
+        contents = old_projects_file.read()
+        contents = frontmatter.filter_fields(contents,
+                                             field_whitelist=['title'])
 
     new_projects_path = os.path.join(new_root, 'content', 'projects.md')
     with open(new_projects_path, 'w') as new_projects_file:
-        new_projects_file.write(new_contents)
+        new_projects_file.write(contents)
